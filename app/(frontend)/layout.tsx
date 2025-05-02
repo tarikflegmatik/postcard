9ca94266d24e3da2438001062e9969362f8f2afb
Tarik Besic <tarik@flegmatik.hr>
2025-05-02 10:06:02 +0200

    re-enable splash screen

--- app/(frontend)/layout.tsx
+++ app/(frontend)/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${poppins.className} antialiased`}>
-        {/*<SplashScreen />*/}
+        <SplashScreen />
         <Navbar />
         {children}
       </body>
