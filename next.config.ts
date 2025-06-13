f6fd5409d36f0e76b1b9271a15785f2d2995f6d4
Tarik Besic <tarik@flegmatik.hr>
2025-06-13 17:56:10 +0200

    adjust the image cache

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   images: {
     remotePatterns: [new URL(process.env.S3_HOSTNAME! + "/**")],
+    minimumCacheTTL: 2678400, // 31 days
   },
 };
 
